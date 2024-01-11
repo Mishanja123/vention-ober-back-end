6af3a54520bf6bfad93952141cb04014209306a2
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-01-11 10:40:08 +0100

    ob108:init-created Dockerfile for BE

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize("obar_be", "postgres", "admin", {
   dialect: "postgres",
-  host: "localhost",
+  host: "host.docker.internal", //host.docker.internal
   port: 5433,
 });
 

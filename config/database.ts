d5d47b7326c09896f358f97d6dbb46b8a0fd9227
Misha <shevchenkom606@gmail.com>
2024-01-15 20:11:23 +0100

    ob104:fix-

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("obar_be", "postgres", "26031996Vbif", {
+const sequelize = new Sequelize("obar_be", "postgres", "admin", {
   dialect: "postgres",
   host: "host.docker.internal", //host.docker.internal
-  port: 5432,
+  port: 5433,
 });
 
 export default sequelize;

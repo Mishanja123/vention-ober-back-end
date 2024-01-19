ff83b21fcd3d403d19167de7243bea176fffe899
Hennadii Borysevych <genuch3@gmail.com>
2024-01-19 01:12:34 +0100

    ob128:feat-images to dishes order

--- server.ts
+++ server.ts
@@ -77,9 +77,9 @@ app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
 const startServer = async () => {
   try {
     await sequelize.sync({ force: false });
-    console.log("Database synchronization successful");
     app.listen(PORT, () => {
       console.log("Server is running on port " + PORT);
+      
     });
   } catch (error) {
     console.error("Error synchronizing database:", error);

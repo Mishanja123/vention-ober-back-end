5e53aed0b8e12135be0874ec6eae5d9b1bd8744c
Misha <shevchenkom606@gmail.com>
2024-01-20 14:15:04 +0100

    ob130:fix-from sing to sign

--- app/routes/auth.ts
+++ app/routes/auth.ts
@@ -4,8 +4,8 @@ import authenticate from "../middleware/authenticate";
 
 export function router(app: Express) {
   app
-    .post("/api/auth/sign-up", authController.postSingUp)
-    .post("/api/auth/login", authController.postSingIn)
+    .post("/api/auth/sign-up", authController.postSignUp)
+    .post("/api/auth/login", authController.postSignIn)
     .get("/api/auth/current-user", authenticate, authController.getCurrentUser)
     .get("/api/auth/refresh", authController.getRefreshTokens)
     .get("/api/auth/logout", authController.postLogOut);

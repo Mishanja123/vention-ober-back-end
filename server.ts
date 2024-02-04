f0f7ebec41a9bd4995e66296022d12025bb3ddb5
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-02-04 13:01:23 +0100

    ob195-fixed checkout process be part

--- server.ts
+++ server.ts
@@ -54,7 +54,7 @@ app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
 
 const startServer = async () => {
   try {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
     app.listen(PORT, () => {
       console.log("Server is running on port " + PORT);
     });

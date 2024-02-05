05e62eefa43bd41b8e63a88ba331a345870455b7
Hennadii Borysevych <genuch3@gmail.com>
2024-02-05 12:24:16 +0100

    ob174:fix-added logic to order canceled

--- app/services/orderService/deleteOrderById.ts
+++ app/services/orderService/deleteOrderById.ts
@@ -1,10 +1,20 @@
 import { OrderStatus } from "../../enums/Order";
+import Cart from "../../models/cart";
 import Order from "../../models/order";
 
-export const deleteOrderById = async (id: string) => {
+export const deleteOrderById = async (id: string, userId: number) => {
   const existedOrder = await Order.findByPk(id);
 
   if (existedOrder) {
     await existedOrder.update({ status: OrderStatus.Canceled });
   }
+
+  await Cart.update(
+    {
+      total: 0,
+      subTotal: 0,
+      dishes: [],
+    },
+    { where: { userId } }
+  );
 };

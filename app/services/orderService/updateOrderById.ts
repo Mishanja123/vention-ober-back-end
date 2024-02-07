2408326adfd2d1db896dba7bafba68e16b726615
Misha <shevchenkom606@gmail.com>
2024-02-08 00:12:40 +0100

    ob205:fix-updateOrder

--- app/services/orderService/updateOrderById.ts
+++ app/services/orderService/updateOrderById.ts
@@ -5,20 +5,16 @@ import Order from "../../models/order";
 export const updateOrderById = async (
   id: string,
   userId: number,
-  status: string
+  status: OrderStatus
 ) => {
   const existedOrder = await Order.findByPk(id);
 
-  // if (existedOrder) {
-  //   await existedOrder.update({ status: OrderStatus.Canceled });
-  // }
-
   if (existedOrder) {
     switch (status) {
-      case "completed":
+      case OrderStatus.Completed:
         await existedOrder.update({ status: OrderStatus.Completed });
         break;
-      case "canceled":
+      case OrderStatus.Canceled:
         await existedOrder.update({ status: OrderStatus.Canceled });
         break;
       default:

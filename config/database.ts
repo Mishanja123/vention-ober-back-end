a638003ef3c37a7e5e9c3ddb0e2db1e2566af5ee
Misha <shevchenkom606@gmail.com>
2024-02-10 20:11:43 +0100

    update database

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("obar_be", "postgres", "26031996Vbif", {
+const sequelize = new Sequelize("obar_be", "postgres", "admin", {
   dialect: "postgres",
   host: "localhost",
-  port: 5432,
+  port: 5433,
 });
 
 export default sequelize;

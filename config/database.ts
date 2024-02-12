c7c2c31af08fc698773af8734f5aa6d4d039dc2a
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-02-12 12:09:10 +0100

    changed port for production

--- config/database.ts
+++ config/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize("obar_be", "postgres", "admin", {
   dialect: "postgres",
   host: "localhost",
-  port: 5433,
+  port: 5432,
 });
 
 export default sequelize;
